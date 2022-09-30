75654d5a63675f6b53a4e5f4a8f4b5e1dd6f6451
ui-cod-tv <uicodetv@googlemail.com>
2022-09-30 11:35:32 +0100

    Starter files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="preview">
+
+      </div>
+      <div className="footer">
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isOpen}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
     </div>
+  
   );
 }
 
